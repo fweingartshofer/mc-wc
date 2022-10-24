688e217a431f0098193cb90dae609f319334c04f
florian weingartshofer <florian.weingartshofer@tuta.io>
2022-10-24 09:05:20 +0200

    Missing nelder-mead function

--- pedometer/lib/main.dart
+++ pedometer/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:pedometer/math_utils.dart';
+import 'package:fmin/fmin.dart';
+import 'package:pedometer/math_utils.dart' as utils;
 import 'package:pedometer/tuples.dart';
+import 'package:scidart/numdart.dart';
+import 'package:scidart/scidart.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
@@ -29,10 +32,12 @@ class Counter extends StatefulWidget {
 }
 
 class _CounterState extends State<Counter> {
-  double x = 0, y = 0, z = 0;
+  double x = 0,
+      y = 0,
+      z = 0;
   String direction = "none";
-  List<TimestampedMetrics> axisData =
-      List.empty(growable: true);
+  int stepCount = 0;
+  List<TimestampedMetrics> axisData = List.empty(growable: true);
 
   @override
   void initState() {
@@ -42,35 +47,55 @@ class _CounterState extends State<Counter> {
       z = event.z;
       axisData.add(TimestampedMetrics(DateTime.now(), x, y, z));
 
-      int samplingFreq = 100;
-      int windowSize = 320;
-      double duration = 1.25;
-      double resolution = samplingFreq / windowSize;
-      double length = duration * samplingFreq;
-
-      int i = 0;
-      while (i + windowSize < axisData.length) {
-        Tuple3<num, num, num> mean = axisData
-            .map((e) => e.map((p0) => p0.abs()))
-            .reduce((value, element) => value + element) / axisData.length;
-        int maxAxis = max(mean);
-      }
-
-      /*
-      if (x > 0) {
-        direction = "back";
-      } else if (x < 0) {
-        direction = "forward";
-      }
-      if (y > 0) {
-        direction += " left";
-      } else if (y < 0) {
-        direction += " right";
-      }*/
+      _calculateSteps();
 
       setState(() {});
     });
     super.initState();
+  }
+
+  int _calculateSteps() {
+    int samplingFreq = 100;
+    int windowSize = 320;
+    double duration = 1.25;
+    double resolution = samplingFreq / windowSize;
+    double length = duration * samplingFreq;
+    
+    int i = 0;
+    while (i + windowSize < axisData.length) {
+      var subAxisData = axisData.sublist(i, i + windowSize);
+      Tuple3<double, double, double> meanOfAxis = subAxisData
+          .map((e) => e.map((p0) => p0.abs()))
+          .reduce((value, element) => value + element) /
+          axisData.length;
+    
+      int maxAxis = utils.max(meanOfAxis);
+      Iterable<double> maxAxisData = subAxisData.map((t) => t[maxAxis]);
+    
+      List<double> s = arrayComplexAbs(
+          fft(arrayToComplexArray(Array(maxAxisData.toList()))))
+          .map((element) => element * 2)
+          .toList();
+
+      var yPolyAxis = Array(s.sublist(2, 7));
+      var xPolyAxis = Array([1, 2, 3, 4, 5]);
+
+      var w0 = mean(Array(s.sublist(0, 2)));
+      var wc = mean(yPolyAxis);
+    
+      var coefficients = PolyFit(xPolyAxis, yPolyAxis, 4);
+      double f(List<double> x) => coefficients.coefficient(x.first.toInt());
+      var max = 0;
+      if (wc > w0 && wc > 10) {
+        var fw = resolution * (max + 1);
+        var c = duration * fw;
+        stepCount += (stepCount + c).toInt();
+      }
+
+      i += length as int;
+    }
+
+    return 0;
   }
 
   @override
@@ -85,7 +110,7 @@ class _CounterState extends State<Counter> {
           padding: const EdgeInsets.all(30),
           child: Column(children: [
             Text(
-              direction,
+              "Step Count: $stepCount",
               style: const TextStyle(fontSize: 30),
             )
           ])),
--- pedometer/lib/tuples.dart
+++ pedometer/lib/tuples.dart
@@ -16,20 +16,36 @@ class Tuple4<T0, T1, T2, T3> {
 }
 
 class TimestampedMetrics {
-  Tuple4<DateTime, num, num, num> data;
-  TimestampedMetrics(DateTime timestamp, num n1, num n2, num n3): data = Tuple4(timestamp, n1, n2, n3);
+  Tuple4<DateTime, double, double, double> data;
+
+  TimestampedMetrics(DateTime timestamp, double n1, double n2, double n3)
+      : data = Tuple4(timestamp, n1, n2, n3);
 
   /// Returns a TimeseriesMetrics where the Timeaxis is copied and the other metrics are combined
   TimestampedMetrics operator +(TimestampedMetrics other) {
-    return TimestampedMetrics(data.d0, data.d1 + other.data.d1, data.d2 + other.data.d2, data.d3 + other.data.d3);
+    return TimestampedMetrics(data.d0, data.d1 + other.data.d1,
+        data.d2 + other.data.d2, data.d3 + other.data.d3);
   }
 
   /// Returns a Tuple3 ignoring the time axis
-  Tuple3<num, num, num> operator /(num divisor) {
-    return Tuple3(data.d1/divisor, data.d2/divisor, data.d3/divisor);
+  Tuple3<double, double, double> operator /(num divisor) {
+    return Tuple3(data.d1 / divisor, data.d2 / divisor, data.d3 / divisor);
   }
 
-  TimestampedMetrics map(Function(num) f) {
+  double operator [](int idx) {
+    switch (idx) {
+      case 0:
+        return data.d1;
+      case 1:
+        return data.d2;
+      case 2:
+        return data.d3;
+      default:
+        throw Error();
+    }
+  }
+
+  TimestampedMetrics map(Function(double) f) {
     return TimestampedMetrics(data.d0, f(data.d1), f(data.d1), f(data.d1));
   }
 }
